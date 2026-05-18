bc7b09019adef6a38a7879472bad50662f15c1de
Anastasiia <anastasiia.remyzovska@gmail.com>
2026-05-18 18:44:57 +0300

    added new chages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const revealElements = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  revealElements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < window.innerHeight) {
+      element.classList.add('reveal--visible');
+    }
+  });
+});
